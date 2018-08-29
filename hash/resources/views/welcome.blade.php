38a2c9ee32b6595d5bcba618f6aeccbb79fbf1c0
Olatunji Joshua <19627177+OkayJosh@users.noreply.github.com>
2018-08-29 23:38:49 +0100

    addig a simple about context

--- hash/resources/views/welcome.blade.php
+++ hash/resources/views/welcome.blade.php
@@ -64,7 +64,21 @@
                 <div class="content">
                     <div class="title">Olatunji joshua</div>
                 </div>
-                <div class="links">
+                <div class="content"> <p>My name is joshua kayode, 
+                i strongly believe in the development and advancement of 
+                the collective intelligence of the human race, the sum total 
+                of all intelligent mind that have been here on the planet up till now,
+                 their contribution and impact. if we could further that, 
+                 improve on their strive, we will only cause intelligent advancement
+                  to our own species, will will further our biology, our planet will
+                   be peaceful, and we will eradicate suffering and all it vices.
+                My area of interest spans from Artificial intelligence,
+                philosophy to spirituality of the modern age. 
+                This area of interest has a strong pull on me because i see them as
+                 providing a powerful tool that empowers the mind, seeing the future
+                  from an optimistic position, and allowing us to take control of our 
+                  own destiny as a specie. </p> </div>
+                <div class="links flex-center">
                         <a href="#">blog</a>
                         <a href="#">Projects</a>
                         <a href="#">Futurist</a>
